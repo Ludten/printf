1db7e3f297c0819bb626d696ebcfce68b2600d97
SlamChillz <mendyslam@gmail.com>
2022-04-20 07:16:17 +0000

    Header file adjusted to betty standard

--- main.h
+++ main.h
@@ -24,15 +24,20 @@ void getwidth(const char **fmt, va_list args, int *field_width, int *flags);
 void getprecision(const char **fmt, va_list args, int *precision);
 void getqualifier(const char **fmt, int *qualifier);
 void c(char **str, va_list args, int *flags, int *field_width);
-void s(char **str, va_list args, int *flags, int *field_width, int *precision, int *len);
-void r(char **str, va_list args, int *flags, int *field_width, int *precision, int *len);
-void R(char **str, va_list args, int *flags, int *field_width, int *precision, int *len);
+void s(char **str, va_list args, int *flags,
+		int *field_width, int *precision, int *len);
+void r(char **str, va_list args, int *flags,
+		int *field_width, int *precision, int *len);
+void R(char **str, va_list args, int *flags,
+		int *field_width, int *precision, int *len);
 void p(char **str, va_list args, int *field_width, int *flags, int *precision);
 void lh(va_list args, int *flags, int *qualifier, unsigned long *num);
 char *number(char *str, long num, int base, int size, int precision, int type);
 void checktype(int *type, long *num, int *size, char *sign, int *base);
-void checkprecision(char **str, int *type, int *precision, int *i, int *size, char *sign, int *base, char *c, char *locase);
-int customspecifiers1(const char **fmt, char **str, va_list args, int *flags, int *field_width, int *precision, int *len);
+void checkprecision(char **str, int *type, int *precision, int *i,
+		int *size, char *sign, int *base, char *c, char *locase);
+int customspecifiers1(const char **fmt, char **str, va_list args,
+		int *flags, int *field_width, int *precision, int *len);
 int customspecifiers2(const char **fmt, char **str, int *flags, int *base);
 
 #endif /* PRINTF */
